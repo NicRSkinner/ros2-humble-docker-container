4a1d215f1d639883017ceb0a2ddbf568eaf7a4a9
NicRSkinner <nicholas.skinner95@gmail.com>
2023-08-19 21:05:59 -0500

    Updates for trying to run on Windows.

--- run_ros2_windows.sh
+++ run_ros2_windows.sh
@@ -1,5 +1,14 @@
 #XAUTH=/home/nskinner/.Xauthority
 
-docker run -it --net=host --privileged -v C:/Users/nicho/Documents/ardak/:/root/dd_ws/ardak -v C:/Users/nicho/Documents/ros-inc/:/opt/ros/humble/cp dockros bash
+ docker run -it --privileged \
+		    -v /tmp/.X11-unix:/tmp/.X11-unix \
+                -v /mnt/wslg:/mnt/wslg \
+                -e DISPLAY \
+                -e WAYLAND_DISPLAY \
+                -e XDG_RUNTIME_DIR \
+                -e PULSE_SERVER \
+                dockros bash
+
+#docker run -it --net=host --privileged -v C:/Users/nicho/Documents/ardak/:/root/dd_ws/ardak -v C:/Users/nicho/Documents/ros-inc/:/opt/ros/humble/cp dockros bash
 
 #    -v /home/nskinner/.gazebo:/root/.gazebo \
